a850652e3e71811a09379fc8c9238adf5f74d5f7
“Iryna <maria14trush@gmail.com>
2023-10-24 17:11:11 -0700

    update ReadMe

---
+++ README.md
@@ -0,0 +1,40 @@
+# Mortgage Calculator
+
+The **Python Mortgage Calculator App** allows users to calculate mortgage payments with ease. This application is built using Python's `Tkinter` GUI library. By entering the loan amount, interest rate, and loan term, users can get a quick estimate of their monthly mortgage payment, helping them understand their financial commitments when considering property purchases.
+
+## Features:
+
+- Simple and user-friendly GUI.
+- Input fields for loan amount, interest rate, and loan term.
+- Displays monthly mortgage payment based on user input.
+- Error handling for incomplete data entries.
+
+## Technical Stack:
+
+- **Programming Language**: Python
+- **GUI Library**: Tkinter
+
+## How to Run:
+
+1. Ensure you have Python installed on your machine. You can download and install Python from [here](https://www.python.org/downloads/).
+2. `Tkinter` is included in the standard library in normal Python installations. If for some reason it's not installed, you can install it using pip:
+
+```
+pip install tk
+```
+
+3. Clone the repository or download the Python script.
+4. Navigate to the directory containing the script using the terminal or command prompt.
+5. Run the script using the command:
+
+```
+python calculator.py
+```
+
+6. The GUI application will launch, and you can start using it.
+
+About Author
+
+Software engineer with a background in the media industry, leadership, and communications. Recently graduated from the Software Engineering Program at Hackbright Academy, where I honed my skills in full-stack development and fundamental computer science concepts.
+
+My technical expertise lies in object-oriented programming languages, particularly in Python. Additionally, I am well-versed in JavaScript, HTML, CSS, and the React framework, enabling me to create responsive and client-oriented user interfaces. My [LinkedIn](https://www.linkedin.com/in/trushmi/)
--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 root = tk.Tk()
 root.geometry("500x500")
+root.title("Your mortgage calculator")
 
 def payment():
     if loan_amount_entry.get() and interest_rate_entry.get() and years_entry.get():
@@ -12,41 +13,35 @@ def payment():
         months = years*12
         payment = loan*monthly_rate/(1-(1+monthly_rate) ** (-months))
         payment= '%.2f' % payment 
-        result_label.config(text=f"The monthly payment is:${payment}")
+        result_label.config(text=f"The monthly payment is: ${payment}")
     else:
-        result_label.config(text="Not enough information. Plese, provide all data")
+        result_label.config(text="Not enough information. Please provide all data.")
 
-root.title("Your mortgage calculator")
-label_frame = tk.LabelFrame(root,text="Calculate your mortgage monthly payment")
-label_frame.pack(pady=30)
+label_frame = tk.LabelFrame(root, text="Calculate your mortgage monthly payment")
+label_frame.pack(pady=30, fill="both", expand=True)
 
 frame = tk.Frame(label_frame)
-frame.pack(pady=20,padx=20)
+frame.pack(pady=20, padx=20)
 
-loan_amount_label = tk.Label(frame,text="Loan amount")
-loan_amount_entry = tk.Entry(frame,font=("Helvetica", 16)) 
-
+loan_amount_label = tk.Label(frame, text="Loan amount")
+loan_amount_entry = tk.Entry(frame, font=("Helvetica", 16))
 loan_amount_label.grid(row=0, column=0)
 loan_amount_entry.grid(row=0, column=1)
 
-interest_rate_label = tk.Label(frame,text="Interest rate")
-interest_rate_entry = tk.Entry(frame,font=("Helvetica", 16)) 
-
+interest_rate_label = tk.Label(frame, text="Interest rate")
+interest_rate_entry = tk.Entry(frame, font=("Helvetica", 16))
 interest_rate_label.grid(row=1, column=0)
-interest_rate_entry.grid(row=1, column=1,pady=20)
+interest_rate_entry.grid(row=1, column=1, pady=20)
 
-
-years_label = tk.Label(frame,text="Loan term: ")
-years_entry = tk.Entry(frame,font=("Helvetica", 16)) 
-
+years_label = tk.Label(frame, text="Loan term: ")
+years_entry = tk.Entry(frame, font=("Helvetica", 16))
 years_label.grid(row=2, column=0)
 years_entry.grid(row=2, column=1)
 
-
-button = tk.Button(label_frame,text="Get Payment",font=("Helvetica",16),cursor="arrow",command=payment)
+button = tk.Button(label_frame, text="Get Payment", font=("Helvetica", 16), cursor="arrow", command=payment)
 button.pack(pady=20)
 
-result_label = tk.Label(root,text="",font=("Helvetica",16))
+result_label = tk.Label(root, text="", font=("Helvetica", 16))
 result_label.pack(pady=20)
 
 root.mainloop()
